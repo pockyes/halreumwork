6ec31d4603da13c5c7196267344bbf39b50cef0f
pockyes <tele1841@gmail.com>
2021-05-24 17:45:42 +0900

    210524 commit

--- index.js
+++ index.js
@@ -4,5 +4,22 @@ console.log(daysOfWeek);
 let Info = {
   name: "areum",
   age: 28,
-  gender: "Female"
+  gender: "Female",
+  favMovies: ["센과 치히로의 행방불명", "원더", "500일의 썸머"],
+  favFood: [
+    {
+      name: "maratang", 
+      fatty: fals
+    }, 
+    {
+      name: "bulgogi Bugger", 
+      fatty: true
+    }
+  ]
 }
+
+console.log(Info.favFood);
+
+Info.age = 27;
+
+console.log(Info.age);
