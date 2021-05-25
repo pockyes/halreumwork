725dd613e17f01d6079e3c3093daba145fe669ff
pockyes <tele1841@gmail.com>
2021-05-25 16:33:10 +0900

    210525 commit

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
-let daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-let Info = {
-  name: "areum",
-  age: 28,
-  gender: "Female",
-  favMovies: ["센과 치히로의 행방불명", "원더", "500일의 썸머"],
-  favFood: [
-    {
-      name: "maratang", 
-      fatty: false
-    }, 
-    {
-      name: "bulgogi Bugger", 
-      fatty: true
-    }
-  ]
+let ask = prompt("Ask Something");
+
+
+if(ask.length > 30) {
+  console.log("말이 많아");
+} else if(20 < ask.length && ask.length < 30) {
+  console.log("딱좋다");
+} else {
+  console.log("말좀해");
 }
-
-
-function sayHello(myName, age) {
- return `Hello! ${myName} How old? ${age}`;
-}
-
-sayHello("아름", 28);
