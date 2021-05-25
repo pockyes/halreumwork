f5bbaf7aeeb72a8b3736586d3ae5828eae5d1b69
pockyes <tele1841@gmail.com>
2021-05-25 17:18:18 +0900

    210525 commit

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-let ask = prompt("Ask Something");
+// let ask = prompt("Ask Something");
 
 
-if(ask.length > 30) {
-  console.log("말이 많아");
-} else if(20 < ask.length && ask.length < 30) {
-  console.log("딱좋다");
-} else {
-  console.log("말좀해");
+// if(ask.length > 30) {
+//   console.log("말이 많아");
+// } else if(20 < ask.length && ask.length < 30) {
+//   console.log("딱좋다");
+// } else {
+//   console.log("말좀해");
+// }
+
+let title = document.querySelector("#title");
+
+let BASE_COLOR = "#000000";
+let OTHER_COLOR = "#81ecec";
+
+function handleClick() {
+  console.log(title.style.color);
 }
+
+function init() {
+  title.style.color = BASE_COLOR;
+  title.addEventListener("click", handleClick);
+}
+
+init();
+
